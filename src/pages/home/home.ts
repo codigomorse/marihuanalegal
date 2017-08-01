106efed4a13292c064413e07c5ae902f54084d7c
codigo <omargonzaleztecheira@gmail.com>
2017-08-01 14:03:21 -0300

    agrega market desde firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import { AngularFireDatabase  } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
@@ -9,50 +10,63 @@ import { Geolocation } from '@ionic-native/geolocation';
 export class HomePage {
   @ViewChild('map') mapElement;
   map:any=null;
+  dataArr=[];
   latitude;
   longitude;
-  constructor(private geolocation: Geolocation,public navCtrl: NavController) {
+  constructor(private afDb: AngularFireDatabase,private geolocation: Geolocation,public navCtrl: NavController) {
   }
   ionViewDidLoad(){
-    
     this.test();
+  }   
+  test(){
+    //baja los datos
+    this.afDb.list("/stock/").subscribe(_data => {
+      this.dataArr = _data;
+      //console.log(this.dataArr);
+    });
+    //geolocaliza
+    this.geolocation.getCurrentPosition().then((resp) => {
+    //console.log(resp.coords.latitude);
+    //console.log(resp.coords.longitude);
+    this.latitude = resp.coords.latitude;
+    this.longitude = resp.coords.longitude;
+    //dibuja el mapa
+    this.initMap();
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
   }
   initMap(){
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: this.latitude, lng :this.longitude},
-            zoom: 14
-           }); 
-        let myLatLng = new google.maps.LatLng(this.latitude,this.longitude);
-        var marker = new google.maps.Marker({
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: this.latitude, lng :this.longitude},
+      zoom: 14
+    }); 
+    var image = "https://cdn3.iconfinder.com/data/icons/map-markers-2-1/512/drugs-32.png";
+    let myLatLng = new google.maps.LatLng(this.latitude,this.longitude);
+    var marker = new google.maps.Marker({
+      map: map,
+      position: myLatLng,
+      title: 'Hello World!'
+    });
+    this.dataArr.forEach(element => {
+      console.log(element.farmacia);
+      let pos = {lat: element.latitud, lng: element.longitud};
+      console.log(pos);
+      let options = {
           map: map,
-          position: myLatLng,
-          title: 'Hello World!'
-        });
-        var image = "https://cdn3.iconfinder.com/data/icons/map-markers-2-1/512/drugs-32.png";
-        var marker = new google.maps.Marker({
-          map: map,
-          position: {lat: -34.9147836, lng :-56.1498222},
+          position: pos,
           icon: image,
-          title: 'Farmacia Caceres'
-        });
-        
-
-  }
-          
-  test(){
-      let watch = this.geolocation.watchPosition();
-      watch.subscribe((data) => {
-          console.log(data);
-          console.log(data.coords.latitude);
-          console.log(data.coords.longitude);
-          this.latitude = data.coords.latitude;
-          this.longitude = data.coords.longitude;
-          this.initMap();
-          // data can be a set of coordinates, or an error (if an error occurred).
-          // data.coords.latitude
-          // data.coords.longitude
+          title: element.farmacia
+      }
+      console.log(options);
+      marker = new google.maps.Marker(options);
+      console.log(marker);
+      let infoWindow = new google.maps.InfoWindow({
+        content: '<h1>'+element.farmacia+'</h1><p>'+element.horario+'</p><p>'+element.stock+'</p>'
       });
-
-  }
-
+      marker.addListener("click", function(){
+        infoWindow.open(map, marker);
+      })
+    }); 
+  }   
 }
